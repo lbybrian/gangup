555346cc7b116fd448cabec063a3161f7233ec96
liubinyuan <liubinyuan@golaxy.cn>
2021-01-15 12:36:49 +0800

    添加词云图组件，作为背景

---
+++ src/views/Drill/wordCloud/index.vue
@@ -0,0 +1,86 @@
+<template>
+	<div>
+		<div id="container" ref="container"></div>
+	</div>
+</template>
+
+<script>
+	import { WordCloud } from '@antv/g2plot';
+
+	export default {
+		name: "wordCloud",
+		props: {
+			dataObj: Object,
+		},
+		data() {
+			return {
+				dataInfo: {
+					data: [],
+					changeSign: false,
+					imageMask: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*07tdTIOmvlYAAAAAAAAAAABkARQnAQ',
+					wordField: 'name',
+					weightField: 'value',
+					colorField: 'name',
+					interactions: [{
+						type: 'element-active'
+					}],
+					state: {
+						active: { // 这里可以设置 active 时的样式
+							style: {
+								lineWidth: 3,
+							},
+							
+						}
+					},
+					wordStyle: {
+						fontFamily: 'Verdana',
+						fontSize: [8, 32],
+					},
+				}
+			}
+		},
+
+		mounted() {
+			this.$nextTick(function() {
+				var tData = this.dataObj || this.dataInfo;
+				this.updateData(tData);
+			});
+		},
+
+		methods: {
+			updateData(newObj) {
+				this._updateData()
+				var elem = this.$refs.container;
+				if(this.wordCloud) {
+					this.wordCloud.destroy();
+				}
+				this.wordCloud = new WordCloud(elem, newObj);
+				//					if(dat.changeSign){
+				this.wordCloud.render();
+
+			},
+			_updateData(newObj) {
+				this.dataInfo.data=[]
+				for(var k in newObj) {
+					this.$set(this.dataInfo, k, newObj[k]);
+					for(var i in newObj.data){
+						this.dataInfo.data.push(newObj[i])
+					}
+				}
+			},
+
+		},
+		created() {},
+	};
+</script>
+
+<style scoped>
+	.containerArea {
+		width: 100%;
+		height: 100%;
+	}
+	
+	/deep/ .el-pagination {
+		padding-left: 0px;
+	}
+</style>
