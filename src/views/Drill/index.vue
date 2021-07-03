bebf57bafac552da7e2996e2313a3d6238e09be9
liubinyuan <lbybrian@163.com>
2021-07-04 00:57:44 +0800

    添加一个上传按钮

--- src/views/Drill/index.vue
+++ src/views/Drill/index.vue
@@ -27,7 +27,19 @@
 	    <el-dialog title="创建" :visible.sync="visible" width="40%" ref="createRuleForm">
 	    	<el-form :model="cFormData" :rule="cRules" lable-width="130px">
 	    		<el-form-item lable="测试" prop>
-	    			
+	    			<el-upload
+    				  class="upload-demo"
+					  action="https://jsonplaceholder.typicode.com/posts/"
+					  :on-preview="handlePreview"
+					  :on-remove="handleRemove"
+					  :before-remove="beforeRemove"
+					  multiple
+					  :limit="3"
+					  :on-exceed="handleExceed"
+					  :file-list="fileList">
+					  <el-button size="small" type="primary">点击上传</el-button>
+					  <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+	    			</el-upload>
 	    		</el-form-item>
 	    	</el-form>
 	    </el-dialog>
@@ -46,6 +58,9 @@
 					id:1,
 					objType:1,
 				},
+				fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}],
+				cFormData:[],
+				cRules:[],
 				dataList:[],
 				typeList:[
 					{
@@ -100,7 +115,22 @@
 		      },
 		      handleDelete(index, row) {
 		        console.log(index, row);
+		      },
+		      //弹窗上传文件相关4个
+		      handleRemove(file, fileList) {
+		        console.log(file, fileList);
+		      },
+		      handlePreview(file) {
+		        console.log(file);
+		      },
+		      handleExceed(files, fileList) {
+		        this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+		      },
+		      beforeRemove(file, fileList) {
+		        return this.$confirm(`确定移除 ${ file.name }？`);
 		      }
+		      
+		      
 		},
 		mounted(){},
 	}
